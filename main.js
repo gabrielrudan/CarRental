0e149595d2f91656a30bd6735b68e14625166317
Gabriel Rudan <gabrielrudam92@gmail.com>
2022-05-11 13:56:18 -0300

    Ajeitando o main

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.set('view engine', 'ejs');
+app.set('views', './view');
 
 app.get('/', (req, res) => {
   res.render('index')
