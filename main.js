45f8642314851fecc442ec29a2a3aec5ba7821d1
Raylander524 <raylandermarques24@gmail.com>
2022-05-11 15:28:34 -0300

    Ação dos botões de login e cadastro

--- main.js
+++ main.js
@@ -2,14 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
+const cookieSession = require('cookie-session')
+
 app.set('view engine', 'ejs');
 app.set('views', './view');
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(cookieSession({
+  name: 'session',
+  // keys: ['c293x8b6234z82n938246bc2938x4zb234'],
+  secret: 'c293x8b6234z82n938246bc2938x4zb234',
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
+
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index');
+})
+
+app.get('/sign-in', (req, res) => {
+  res.render('login_usuario', {title: 'Página de login', pagina:'Página de login'});
+})
+
+app.get('/sign-up', (req, res) => {
+  res.render('cadastrar_usuario',{title: 'Página de Cadastrar', pagina:'Página de Cadastrar'});
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
