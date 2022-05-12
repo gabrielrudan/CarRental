46eb4bbe978362f4e6b3476da65d0809b8c16cc9
Leticia Tores <leticiatp2@gmail.com>
2022-05-12 09:03:55 -0300

    primeira tentativa crud

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+const path = require('path');
+const cookieSession = require('cookie-session')
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = 3000
-const path = require('path');
-const cookieSession = require('cookie-session')
 
 app.set('view engine', 'ejs');
 app.set('views', './view');
+
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(cookieSession({
   name: 'session',
@@ -16,19 +19,55 @@ app.use(cookieSession({
 }))
 
 
+/**Mongo */
+const MongoClient = require('mongodb').MongoClient
+const connctionString = 'mongodb+srv://leh-torres:sweetland@cluster0.uhidp.mongodb.net/car-rental-db?retryWrites=true&w=majority'
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
+MongoClient.connect(connctionString, {
+  useUnifiedTopology: true})
+    .then(client => {
+      console.log('Conexao Estabelecida')
+      const db = client.db('car-rental-db')
+      const frasesCollection = db.collection('frases')
 
-app.get('/sign-in', (req, res) => {
-  res.render('login_usuario', {title: 'Página de login', pagina:'Página de login'});
-})
+      app.get('/', (req, res) => {
+        res.render('index');
+      })
+      
+      app.get('/show', (req, res)=>{
+        db.collection('frases').find().toArray()
+          .then(results => {
+            res.render('show', {frases: results})
+          })
+          .catch(error => console.error(error))
+          
+      })
+      
+      app.post('/show', (req, res) => {
+          frasesCollection.insertOne(req.body)
+          .then(result => {
+            console.log(result)
+            res.redirect('/')
+          })
+          .catch(error => console.error(error))
+      })
+      
+      app.get('/sign-in', (req, res) => {
+        res.render('login_usuario', {title: 'Página de login', pagina:'Página de login'});
+      })
+      
+      app.get('/sign-up', (req, res) => {
+        res.render('cadastrar_usuario',{title: 'Página de Cadastrar', pagina:'Página de Cadastrar'});
+      })
+      
+      app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+      })
 
-app.get('/sign-up', (req, res) => {
-  res.render('cadastrar_usuario',{title: 'Página de Cadastrar', pagina:'Página de Cadastrar'});
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+    })
+    .catch(error => console.error(error))
+
+
+
+
