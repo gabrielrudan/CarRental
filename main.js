9a60b07a93f3de9754918522ee764aa4bd7e2ec4
Leticia Tores <leticiatp2@gmail.com>
2022-05-12 23:46:48 -0300

    main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+const path = require('path');
+const cookieSession = require('cookie-session')
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = 3000
-const path = require('path');
-const cookieSession = require('cookie-session')
 
 app.set('view engine', 'ejs');
 app.set('views', './view');
+
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(cookieSession({
   name: 'session',
@@ -14,21 +17,81 @@ app.use(cookieSession({
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
+app.use(bodyParser.json())
+
+/**Mongo */
+const MongoClient = require('mongodb').MongoClient
+const connctionString = 'mongodb+srv://leh-torres:sweetland@cluster0.uhidp.mongodb.net/car-rental-db?retryWrites=true&w=majority'
+
+MongoClient.connect(connctionString, {
+  useUnifiedTopology: true})
+    .then(client => {
+      console.log('Conexao Estabelecida')
+      const db = client.db('car-rental-db')
+      const frasesCollection = db.collection('frases')
+
+      const usuariosCollection = db.collection('usuarios')
+      const adminsCollection = db.collection('admins')
+      const carsCollection = db.collection('cars')
+
+      app.get('/', (req, res) => {
+        res.render('index');
+      })
+      
+      app.get('/show', (req, res)=>{
+        db.collection('frases').find().toArray()
+          .then(results => {
+            res.render('show', {frases: results})
+          })
+          .catch(error => console.error(error))
+          
+      })
+      
+      app.post('/show', (req, res) => {
+          frasesCollection.insertOne(req.body)
+          .then(result => {
+            console.log(result)
+            res.redirect('/')
+          })
+          .catch(error => console.error(error))
+      })
+
+      app.put('/show', (req, res) => {
+        console.log('app.put')
+        frasesCollection.findOneAndUpdate(
+          {nome: 'Yoda'},
+          {
+            $set:{
+              nome: req.body.name,
+              frase: req.body.quote
+            }
+          },
+          {
+            upsert: true
+          }
+        )
+          .then(result => {
+            console.log(result)
+          })
+          .catch(error => console.error(error))
+      })
+      
+      app.get('/sign-in', (req, res) => {
+        res.render('login_usuario', {title: 'Página de login', pagina:'Página de login'});
+      })
+      
+      app.get('/sign-up', (req, res) => {
+        res.render('cadastrar_usuario',{title: 'Página de Cadastrar', pagina:'Página de Cadastrar'});
+      })
+      
+      app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+      })
+
+
+    })
+    .catch(error => console.error(error))
 
 
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
 
-app.get('/sign-in', (req, res) => {
-  res.render('login_usuario', {title: 'Página de login', pagina:'Página de login'});
-})
-
-app.get('/sign-up', (req, res) => {
-  res.render('cadastrar_usuario',{title: 'Página de Cadastrar', pagina:'Página de Cadastrar'});
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
